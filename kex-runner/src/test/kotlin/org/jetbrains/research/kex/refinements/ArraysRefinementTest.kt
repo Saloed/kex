271f4c196d14e490baaa1b86ab693e5c2dd19cb1
Valentyn Sobol <v.sobol@kspt.icc.spbstu.ru>
2020-03-31 17:25:25 +0300

    test for arrays

--- kex-runner/src/test/kotlin/org/jetbrains/research/kex/refinements/ArraysRefinementTest.kt
+++ kex-runner/src/test/kotlin/org/jetbrains/research/kex/refinements/ArraysRefinementTest.kt
@@ -2,9 +2,9 @@ package org.jetbrains.research.kex.refinements
 
 import org.jetbrains.research.kex.ktype.KexArray
 import org.jetbrains.research.kex.ktype.KexInt
+import org.jetbrains.research.kex.state.basic
 import org.jetbrains.research.kex.state.choice
-import org.jetbrains.research.kex.state.emptyState
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 
 class ArraysRefinementTest : RefinementTest("Arrays") {
     @Test
@@ -16,10 +16,18 @@ class ArraysRefinementTest : RefinementTest("Arrays") {
                 }
             }, {
                 path {
-                    arg(KexInt(), 1) ge arg(KexArray(KexInt()), 1).length() equality const(true)
+                    arg(KexInt(), 1) ge arg(KexArray(KexInt(), memspace = 1), 0).length() equality const(true)
                 }
             })
         }
-        refinement(IllegalArgumentException()) { emptyState() }
+        refinement(IllegalArgumentException()) {
+            basic {
+                path {
+                    val array = arg(KexArray(KexInt(), memspace = 1), 0)
+                    val index = tf.getArrayIndex(array, arg(KexInt(), 1))
+                    tf.getArrayLoad(index) lt const(0) equality const(true)
+                }
+            }
+        }
     }
 }
