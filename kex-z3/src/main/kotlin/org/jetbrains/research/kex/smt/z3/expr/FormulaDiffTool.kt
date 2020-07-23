ab99fe53444e5a5cf647331db28a9fe5e49a9f00
Valentyn Sobol <v.sobol@kspt.icc.spbstu.ru>
2020-07-23 20:34:42 +0300

    remove third-party library for diff tool

--- kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/expr/FormulaDiffTool.kt
+++ kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/expr/FormulaDiffTool.kt
@@ -3,154 +3,96 @@ package org.jetbrains.research.kex.smt.z3.expr
 import com.abdullin.kthelper.collection.dequeOf
 import com.microsoft.z3.Context
 import com.microsoft.z3.Expr
-import de.danielbechler.diff.NodeQueryService
-import de.danielbechler.diff.ObjectDifferBuilder
-import de.danielbechler.diff.access.Accessor
-import de.danielbechler.diff.access.Instances
-import de.danielbechler.diff.differ.Differ
-import de.danielbechler.diff.differ.DifferDispatcher
-import de.danielbechler.diff.node.DiffNode
-import de.danielbechler.diff.node.Visit
-import de.danielbechler.diff.selector.ElementSelector
-import java.util.*
 import kotlin.math.max
+import java.util.*
 
 class FormulaDiffTool(val ctx: Context, val first: Expr, val second: Expr) : Z3ExpressionVisitor {
     fun diff(): Expr {
-        val diff = exprDiffTool().compare<Expr>(first, second)
-        val changed = ChangedNodesCollector.changedNodes(diff)
+        val diffNodes = diff(null, null, first, second).terminalNodes().filterIsInstance<DiffNode.Different>()
         var result = first
-        for (change in changed) {
-            result = applyChange(result, change)
+        for (node in diffNodes) {
+            val path = node.path()
+            val firstExpr = path.eval(first)
+            val secondExpr = path.eval(second)
+            val diffFunDecl = ctx.mkFreshFuncDecl("diff", arrayOf(firstExpr.sort, secondExpr.sort), firstExpr.sort)
+            val diffApp = ctx.mkApp(diffFunDecl, firstExpr, secondExpr)
+            result = path.replaceAtPath(result, diffApp)
         }
-        return result
+        return result.simplify()
     }
 
-    private fun applyChange(rootExpr: Expr, change: DiffNode): Expr {
-        val path = change.path.elementSelectors.filterIsInstance<ExprArgElementSelector>()
-        val pathElements = dequeOf(path)
-        return applyChange(rootExpr, change, pathElements)
-    }
-
-    private fun applyChange(rootExpr: Expr, change: DiffNode, path: Deque<ExprArgElementSelector>): Expr {
-        if (path.isEmpty()) {
-            val firstExpr = change.canonicalGet(first) as? Expr ?: ctx.mkFreshConst("nothing", rootExpr.sort)
-            val secondExpr = change.canonicalGet(second) as? Expr ?: ctx.mkFreshConst("nothing", rootExpr.sort)
-            val diff = ctx.mkFreshFuncDecl("diff", arrayOf(firstExpr.sort, secondExpr.sort), rootExpr.sort)
-            return ctx.mkApp(diff, firstExpr, secondExpr)
-        }
-        val current = path.removeFirst()
-        val args = rootExpr.args
-        val updated = applyChange(args[current.index], change, path)
-        args[current.index] = updated
-        return rootExpr.update(args)
-    }
-
-    companion object {
-        private fun exprDiffTool() = ObjectDifferBuilder
-                .startBuilding()
-                .differs().register { dispatcher, nodeQueryService -> ExprDiffer(dispatcher, nodeQueryService) }
-                .build()
-    }
-}
-
-private class ExprDiffer(val dispatcher: DifferDispatcher, val nodeQueryService: NodeQueryService) : Differ {
-    override fun accepts(type: Class<*>): Boolean = Expr::class.java.isAssignableFrom(type)
-    override fun compare(parent: DiffNode?, instances: Instances): DiffNode {
-        val node = DiffNode(parent, instances.sourceAccessor, instances.type)
-        compareInstances(node, instances)
-        return node
-    }
-
-    private fun compareInstances(node: DiffNode, instances: Instances) {
-        val first = instances.base as Expr?
-        val second = instances.working as Expr?
-        when {
-            first == null && second == null -> {
-                node.state = DiffNode.State.UNTOUCHED
-                return
+    private fun diff(parent: DiffNode?, selector: ExprSelector?, first: Expr, second: Expr): DiffNode = when {
+        first == second -> DiffNode.Same(parent, selector)
+        first.astKind != second.astKind -> DiffNode.Different(parent, selector)
+        !first.isExpr -> DiffNode.Different(parent, selector)
+        first.sort != second.sort -> DiffNode.Different(parent, selector)
+        first.isApp && first.funcDecl != second.funcDecl -> DiffNode.Different(parent, selector)
+        else -> {
+            val node = DiffNode.Different(parent, selector)
+            val firstNumArgs = max(0, first.numArgs)
+            val secondNumArgs = max(0, first.numArgs)
+            if (firstNumArgs != secondNumArgs) throw IllegalStateException("Same functions with different arguments number")
+            for (argIndex in 0 until firstNumArgs) {
+                val exprSelector = ExprSelector(argIndex)
+                node.children += diff(node, exprSelector, exprSelector.get(first), exprSelector.get(second))
             }
-            first == null || second == null -> {
-                node.state = DiffNode.State.CHANGED
-                return
-            }
-            else -> compareExprs(node, first, second, instances)
-        }
-    }
-
-    private fun compareExprs(node: DiffNode, first: Expr, second: Expr, instances: Instances) {
-        when {
-            first == second -> {
-                node.state = DiffNode.State.UNTOUCHED
-                return
-            }
-            first.astKind != second.astKind -> {
-                node.state = DiffNode.State.CHANGED
-                return
-            }
-            !first.isExpr -> {
-                node.state = DiffNode.State.CHANGED
-                return
-            }
-            first.sort != second.sort -> {
-                node.state = DiffNode.State.CHANGED
-                return
-            }
-            first.isApp && first.funcDecl != second.funcDecl -> {
-                node.state = DiffNode.State.CHANGED
-                return
-            }
-            else -> {
-                val numArgs = max(0, max(first.numArgs, second.numArgs))
-                if (numArgs == 0) {
-                    node.state = DiffNode.State.CHANGED
-                    return
-                }
-                for (argIndex in 0..numArgs) {
-                    val child = dispatcher.dispatch(node, instances, ExprArgAccessor(argIndex))
-                    node.addChild(child)
-                }
-            }
+            node
         }
     }
 }
 
-private interface ExprAccessor : Accessor {
-    fun get(expr: Expr): Expr?
-    fun set(expr: Expr, value: Expr?): Unit
-    fun unset(expr: Expr): Unit
-
-    override fun get(p0: Any?) = when (p0) {
-        null -> null
-        else -> get(p0 as Expr)
+private data class ExprSelector(val argIndex: Int) {
+    fun get(expr: Expr): Expr = when {
+        argIndex >= expr.numArgs -> throw IllegalStateException("Unable to apply $this to $expr ")
+        else -> expr.args[argIndex]
     }
 
-    override fun set(p0: Any?, p1: Any?) = if (p0 != null) set(p0 as Expr, p1 as Expr?) else Unit
-    override fun unset(p0: Any?) = if (p0 != null) unset(p0 as Expr) else Unit
+    fun set(expr: Expr, value: Expr): Expr = when {
+        argIndex >= expr.numArgs -> throw IllegalStateException("Unable to apply $this to $expr ")
+        else -> {
+            val args = expr.args
+            args[argIndex] = value
+            expr.update(args)
+        }
+    }
 }
 
-private class ExprArgAccessor(val index: Int) : ExprAccessor {
-    override fun get(expr: Expr): Expr? = when {
-        index >= expr.numArgs -> null
-        else -> expr.args[index]
+private data class DiffPath(val selectors: List<ExprSelector>) {
+    fun eval(expr: Expr): Expr = selectors.fold(expr) { currExpr, selector -> selector.get(currExpr) }
+    fun replaceAtPath(expr: Expr, newExpr: Expr): Expr {
+        val pathElements = dequeOf(selectors)
+        return replaceAtPath(expr, newExpr, pathElements)
     }
 
-    override fun set(expr: Expr, value: Expr?): Unit = TODO("Not yet implemented")
-    override fun unset(expr: Expr): Unit = TODO("Not yet implemented")
-    override fun getElementSelector(): ElementSelector = ExprArgElementSelector(index)
+    private fun replaceAtPath(expr: Expr, newExpr: Expr, path: Deque<ExprSelector>): Expr = when {
+        path.isEmpty() -> newExpr
+        else -> {
+            val current = path.removeFirst()
+            current.set(expr, replaceAtPath(current.get(expr), newExpr, path))
+        }
+    }
 }
 
-private data class ExprArgElementSelector(val index: Int) : ElementSelector() {
-    override fun toHumanReadableString(): String = "[$index]"
-}
+private sealed class DiffNode(val parent: DiffNode?, val children: MutableList<DiffNode>, val selector: ExprSelector?) {
+    val isTerminal: Boolean
+        get() = children.isEmpty()
 
-private class ChangedNodesCollector : DiffNode.Visitor {
-    val nodes = arrayListOf<DiffNode>()
-    override fun node(node: DiffNode, visit: Visit) {
-        if (node.hasChanges() && !node.hasChildren()) nodes.add(node)
+    class Same(parent: DiffNode?, selector: ExprSelector?) : DiffNode(parent, arrayListOf(), selector)
+    class Different(parent: DiffNode?, selector: ExprSelector?) : DiffNode(parent, arrayListOf(), selector)
+
+    fun path(): DiffPath {
+        val path = arrayListOf<ExprSelector>()
+        var node: DiffNode? = this
+        while (node != null) {
+            val selector = node.selector
+            node = node.parent
+            path += selector ?: continue
+        }
+        return DiffPath(path.reversed())
     }
 
-    companion object {
-        fun changedNodes(root: DiffNode) = ChangedNodesCollector().apply { root.visit(this) }.nodes
+    fun terminalNodes(): List<DiffNode> = when {
+        isTerminal -> listOf(this)
+        else -> children.flatMap { it.terminalNodes() }
     }
 }
