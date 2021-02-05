613c3545ddfc76568d31f9f6284a545550c722e7
saloed <napster_1997@mail.ru>
2021-02-05 20:32:29 +0300

    Move z3 to github packages && smal changes

--- kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/fixpoint/FixpointCallCtxWithFunctionCalls.kt
+++ kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/fixpoint/FixpointCallCtxWithFunctionCalls.kt
@@ -1,8 +1,21 @@
 package org.jetbrains.research.kex.smt.z3.fixpoint
 
+import org.jetbrains.research.kex.smt.z3.Z3OptionBuilder
+import org.jetbrains.research.kex.smt.z3.fixpoint.converter.FixpointExprFactory
 import org.jetbrains.research.kex.smt.z3.fixpoint.query.FixpointSolverQuery
 import org.jetbrains.research.kfg.type.TypeFactory
 
 class FixpointCallCtxWithFunctionCalls(
     tf: TypeFactory, solverQuery: FixpointSolverQuery
-) : FixpointCallCtx(tf, solverQuery)
+) : FixpointCallCtx(tf, solverQuery) {
+    override fun mkExprFactory() = FixpointExprFactory.builder()
+        .withFunctionCallSupport()
+        .withDeclarationsTracking(declarationTracker)
+        .build()
+
+    override fun mkOptions() = Z3OptionBuilder()
+        .fp.engine("spacer")
+        .fp.xform.inlineEager(false)
+        .fp.xform.inlineLinear(false)
+        .fp.spacer.simplifyPob(true)
+}
