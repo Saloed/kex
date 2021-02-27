a0250451e8689ad9b4aa41d830f0f389ac1bbeb6
saloed <napster_1997@mail.ru>
2021-02-27 20:04:20 +0300

    more readable diff for z3 traces

--- kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/traceAnalysis.kt
+++ kex-z3/src/main/kotlin/org/jetbrains/research/kex/smt/z3/traceAnalysis.kt
@@ -1,37 +1,104 @@
 package org.jetbrains.research.kex.smt.z3
 
+import com.github.saloed.diff.DiffMode
+import com.github.saloed.diff.twoWayDiffFromDMPDiff
+import name.fraser.neil.plaintext.DiffMatchPatch
+import name.fraser.neil.plaintext.diff_match_patch.Diff
+import name.fraser.neil.plaintext.diff_match_patch.Operation
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.Options
 import java.nio.file.Paths
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.readLines
+import kotlin.io.path.writeText
 
 @ExperimentalPathApi
 fun main(args: Array<String>) {
     val options = Options()
         .addOption(Option("l", "left", true, "Z3 trace file to place on left side").apply { isRequired = true })
         .addOption(Option("r", "right", true, "Z3 trace file to place on right side").apply { isRequired = true })
+        .addOption(Option("o", "out", true, "Result patch file name").apply { isRequired = true })
     val parsedArgs = DefaultParser().parse(options, args)
-    val lhs = parsedArgs.getOptionValue("left").let { Paths.get(it) }.readLines(Charsets.UTF_8)
-    val rhs = parsedArgs.getOptionValue("right").let { Paths.get(it) }.readLines(Charsets.UTF_8)
+    val outFile = parsedArgs.getOptionValue("out").let { Paths.get(it) }
+    val leftFile = parsedArgs.getOptionValue("left").let { Paths.get(it) }
+    val rightFile = parsedArgs.getOptionValue("right").let { Paths.get(it) }
 
-    val lhsSections = makeSections(lhs)
-    val rhsSections = makeSections(rhs)
-    val sectionGroups = groupSections(lhsSections, rhsSections)
+    val lhsSections = makeSections(leftFile.readLines(Charsets.UTF_8))
+    val rhsSections = makeSections(rightFile.readLines(Charsets.UTF_8))
+
+    val leftHeaders = lhsSections.joinToString("\n") { it.header }
+    val rightHeaders = rhsSections.joinToString("\n") { it.header }
+    val linesDiff = DiffMatchPatch().diffLineLevel(leftHeaders, rightHeaders)
+    val headerDiff = makeHeaderDiff(linesDiff)
+
+    val sectionGroups = groupSections(headerDiff, lhsSections, rhsSections)
     check(sectionGroups.filter { it.left != null }.count() == lhsSections.size) { "Left groups mismatch" }
     check(sectionGroups.filter { it.right != null }.count() == rhsSections.size) { "Right groups mismatch" }
-
-    val a = 3
+    val diffs = sectionGroups.flatMap { it.asDiff() }
+    val twoWayDiff = twoWayDiffFromDMPDiff(diffs, leftFile.fileName.toString(), rightFile.fileName.toString(), DiffMode.LINE)
+    val result = twoWayDiff.saveToJson()
+    outFile.writeText(result, Charsets.UTF_8)
 }
 
-private data class FileSection(val header: String, val content: List<String>)
+private data class HeaderDiff(val left: String?, val right: String?)
 
-private data class SectionGroup(val left: FileSection?, val right: FileSection?)
+
+data class FileSection(val header: String, val content: List<String>) {
+    fun contentString() = content.joinToString(separator = "") { "$it\n" }
+    fun toDiff(operation: Operation) = listOf(Diff(operation, contentString()))
+}
+
+private data class SectionGroup(val left: FileSection?, val right: FileSection?) {
+    fun asDiff(): List<Diff> = when {
+        left != null && right != null -> contentDiff(left, right)
+        left == null && right != null -> right.toDiff(Operation.INSERT)
+        left != null && right == null -> left.toDiff(Operation.DELETE)
+        else -> emptyList()
+    }
+}
+
+private fun contentDiff(left: FileSection, right: FileSection) =
+    DiffMatchPatch().diffLineLevel(left.contentString(), right.contentString())
 
 private val headerLineRegex = Regex("^-------- \\[[\\w\\d_-]+\\] .* ---------$")
 
 private fun String.isHeaderLine() = headerLineRegex.matches(this)
+
+private fun Diff.lines() = text.lines().dropLastWhile { it.isEmpty() }
+
+private fun makeHeaderDiff(diff: List<Diff>): List<HeaderDiff> {
+    val result = mutableListOf<HeaderDiff>()
+    for (diffItem in diff) {
+        val headers = diffItem.lines()
+        result += headers.map {
+            when (diffItem.operation) {
+                Operation.EQUAL -> HeaderDiff(it, it)
+                Operation.DELETE -> HeaderDiff(it, null)
+                Operation.INSERT -> HeaderDiff(null, it)
+            }
+        }
+    }
+    return result
+}
+
+private fun groupSections(
+    diff: List<HeaderDiff>,
+    leftSections: List<FileSection>,
+    rightSections: List<FileSection>
+): List<SectionGroup> {
+    val leftIter = leftSections.iterator()
+    val rightIter = rightSections.iterator()
+    return diff.map { item ->
+        val left = item.left?.let { header ->
+            leftIter.next().also { check(it.header == header) { "Left header mismatch: $header | ${it.header}" } }
+        }
+        val right = item.right?.let { header ->
+            rightIter.next().also { check(it.header == header) { "Right header mismatch: $header | ${it.header}" } }
+        }
+        SectionGroup(left, right)
+    }
+}
 
 private fun makeSections(lines: List<String>): List<FileSection> {
     val sections = mutableListOf<FileSection>()
@@ -56,70 +123,4 @@ private fun makeSections(lines: List<String>): List<FileSection> {
         sections += FileSection(currentHeader, currentSectionContent.toList())
     }
     return sections
-}
-
-private const val MATCH_SEARCH_WINDOW = 10
-
-private fun groupSections(left: List<FileSection>, right: List<FileSection>): List<SectionGroup> {
-    val groups = mutableListOf<SectionGroup>()
-    var leftIdx = 0
-    var rightIdx = 0
-    while (leftIdx < left.size || rightIdx < right.size) {
-        if (leftIdx == left.size) {
-            groups += SectionGroup(null, right[rightIdx])
-            rightIdx++
-            continue
-        }
-        if (rightIdx == right.size) {
-            groups += SectionGroup(left[leftIdx], null)
-            leftIdx++
-            continue
-        }
-        if (left[leftIdx].header == right[rightIdx].header) {
-            groups += SectionGroup(left[leftIdx], right[rightIdx])
-            leftIdx++
-            rightIdx++
-            continue
-        }
-        val rightMatchIdx = searchInWindow(left[leftIdx], rightIdx, right)
-        val leftMatchIdx = searchInWindow(right[rightIdx], leftIdx, left)
-        if (rightMatchIdx == null && leftMatchIdx == null) {
-            groups += SectionGroup(null, right[rightIdx])
-            groups += SectionGroup(left[leftIdx], null)
-            leftIdx++
-            rightIdx++
-            continue
-        }
-        if (rightMatchIdx == null || leftMatchIdx != null && leftMatchIdx - leftIdx <= rightMatchIdx - rightIdx) {
-            // match future record from left to current right
-            while (leftIdx < leftMatchIdx!!) {
-                groups += SectionGroup(left[leftIdx], null)
-                leftIdx++
-            }
-            groups += SectionGroup(left[leftIdx], right[rightIdx])
-            leftIdx++
-            rightIdx++
-            continue
-        } else {
-            // match future record from right to current left
-            while (rightIdx < rightMatchIdx) {
-                groups += SectionGroup(null, right[rightIdx])
-                rightIdx++
-            }
-            groups += SectionGroup(left[leftIdx], right[rightIdx])
-            leftIdx++
-            rightIdx++
-            continue
-        }
-    }
-    return groups
-}
-
-private fun searchInWindow(target: FileSection, startIdx: Int, sections: List<FileSection>): Int? {
-    var idx = startIdx
-    while (idx < minOf(startIdx + MATCH_SEARCH_WINDOW, sections.size)) {
-        if (sections[idx].header == target.header) return idx
-        idx++
-    }
-    return null
 }
